6a31d63bdeaddf4749a351f50bcbd1a4bfe3f28a
Robert F Cooper <rfcooper@sas.upenn.edu>
2016-05-12 12:06:27 -0400

    Finalized temporal culler. Allows fast cropping and trimming of temporal sequences.

--- AOTemporalAnalysis/Temporal_Series_Culler/linkedImageFrameSingle.m
+++ AOTemporalAnalysis/Temporal_Series_Culler/linkedImageFrameSingle.m
@@ -1,4 +1,4 @@
-function [ update, selected, multihand ] = linkedImageFrameSingle( images, titles )
+function [ selected, cropregion ] = linkedImageFrameSingle( images, titles )
 % FUNCTION [ selectionid ] = linkedImageFrameSingle( images, titles )
 % Robert Cooper 05-10-2016
 %   This frame allows a user to select a single image from a group of
@@ -32,7 +32,7 @@ function [ update, selected, multihand ] = linkedImageFrameSingle( images, title
 %
 
 multihand = figure('name',['Press Enter To Include, or Tab to exclude.']);
-update = @updateContents;
+% update = @updateContents;
 set(multihand,'KeyPressFcn',@keypress);
 set(multihand,'HitTest','off');
 
@@ -46,18 +46,43 @@ imsizes = cell2mat(cellfun(@size,images,'UniformOutput',0));
 sliderwidth=0.02;
 
 imfigs = imagesc(images{1}); colormap gray; axis image; axis off; title(titles{1},'Interpreter', 'none'); hold on;
-imrect = rectangle('Position',[1 1 size(images{1},2)-1 size(images{1},1)-1],'LineWidth',4,'EdgeColor','w');
+imrect = rectangle('Position',[1 1 size(images{1},2)-1 size(images{1},1)-1],'LineWidth',3,'EdgeColor','g');
 maskpoly = plot(-1,-1,'LineWidth',2,'Color','b'); hold off;
 set(imfigs,'ButtonDownFcn',@butdwn);
 
 numims = length(images);
 
 if numims > 1
-    sliderui = uicontrol(multihand, 'Style', 'slider','Min',1,'Max', numims,'Value', numims,'SliderStep',[ 1/(numims-1) 1/(numims-1) ],'Units','normalized','Position',[1-sliderwidth 0 sliderwidth 1 ], 'Callback',{@sliderMotion});
+    sliderui = uicontrol(multihand, 'Style', 'slider','Min',1,'Max', numims,'Value', 1,'SliderStep',[ 1/(numims-1) 1/(numims-1) ],'Units','normalized','Position',[1-sliderwidth 0 sliderwidth 1 ], 'Callback',{@sliderMotion});
 end
 %% Create the image viewer
-selectionid = length(images);
-selected = -ones(length(images),1);
+selectionid = 1; %length(images);
+selected = ones(length(images),1);
+
+mask = ones(size(images{1}));
+summask = zeros(size(images{1}));
+maskslices = cell(length(images),1);
+
+% Make all of the masks.
+for m=1:length(selected)           
+    % Make a convex hull mask from each image
+    out = regionprops(images{m}>1,'ConvexHull');
+    out = {out.ConvexHull};
+    out = cell2mat(out');
+
+    if size(out,1) ~= 0 && size(out,2) ~= 0
+        convregion = convhull(out(:,1), out(:,2));
+        convregion = [out(convregion,1), out(convregion,2)];
+
+        maskslices{m} = poly2mask(convregion(:,1),convregion(:,2),...
+                            size(images{m},1), size(images{m},2));
+        summask = summask + maskslices{m};
+    end    
+end
+
+% figure(499); imagesc(summask);
+
+suggestMask()
 
 uiwait(multihand);
 
@@ -104,7 +129,20 @@ uiwait(multihand);
 %         get(gcf,'CurrentCharacter')
         
         if strcmp(get(gcf,'CurrentCharacter'),char(13)) % Character 13 is the enter character...
-            acceptlayer()
+%             acceptlayer()
+
+            % Find the largest incribed rectangle in our current mask.
+            [C, h, w, mask] =FindLargestRectangles(mask,[1 1 0], [300 150]);
+            
+            % Find the coordinates for each corner of the rectangle, and
+            % return them
+            cropregion = regionprops(mask,'ConvexHull');
+            cropregion = cropregion.ConvexHull;
+            % TL TR BR BL
+            cropregion = [floor(min(cropregion)); [ceil(max(cropregion(:,1))) floor( min(cropregion(:,2))) ]; ...
+                          ceil(max(cropregion));  [floor(min(cropregion(:,1))) ceil( max(cropregion(:,2)))] ];
+            selected = logical(selected);
+            uiresume;
         elseif strcmp(get(gcf,'CurrentCharacter'),'r') % r for reject
             rejectlayer()
         elseif strcmp(evt.Key,'uparrow')
@@ -121,52 +159,51 @@ uiwait(multihand);
 
     function suggestMask()
         mask = ones(size(images{1}));
-        for i=1:length(selected)           
-            if selected(i) == 1
-                
-                % Make a convex hull mask from the accepted image
-                out = regionprops(images{i}>1,'ConvexHull');
-                tmpmask = poly2mask(out.ConvexHull(:,1),out.ConvexHull(:,2), size(images{i},1), size(images{i},2));
-                
-                
-                mask = mask.*tmpmask;
-            end            
-        end
-        maskhull = regionprops(mask,'ConvexHull');
         
-        set(maskpoly,'XData', maskhull.ConvexHull(:,1));
-        set(maskpoly,'YData', maskhull.ConvexHull(:,2));
-        % Use for final masking?
-%         [C, h, w, m] =FindLargestRectangles(tmpmask,[1 1 0], [300 150]);
+        for i=1:length(maskslices)
+            if selected(i) == 1 && ~isempty(maskslices{i})
+                mask = mask.*maskslices{i};
                 
+            end
+        end
+        
 %         figure(500); imagesc(mask); colormap gray;
+        if any( mask(:) ~=0 )
+            
+            maskhull = regionprops(mask,'ConvexHull');
+
+            set(maskpoly,'XData', maskhull.ConvexHull(:,1), 'YData', maskhull.ConvexHull(:,2));
+            drawnow;
+            % Use for final masking?
+        end
     end
 
     function acceptlayer()
                
         selected(selectionid) = 1;
-        suggestMask()
+        
         
         if (selectionid+1) <= length(images)
             butdwn([], selectionid+1)
         end
         
-        if( all(selected > -1) )            
-            uiresume;
-        end
+%         if( all(selected > -1) )            
+%             uiresume;
+%         end
     end
 
     function rejectlayer()
                      
         selected(selectionid) = 0;
-
-        if (selectionid-1) > 0
-            butdwn([], selectionid-1)
+        
+        if (selectionid+1) <= length(images)
+            suggestMask()
+            butdwn([], selectionid+1)
         end
   
-        if( all(selected > -1) )            
-            uiresume;
-        end
+%         if( all(selected > -1) )            
+%             uiresume;
+%         end
     end
 
     function butdwn(src, evt)
@@ -191,6 +228,7 @@ uiwait(multihand);
         set(sliderui,'Value', selectionid )
         
         set(imfigs,'CData',images{selectionid});
+        title(titles{selectionid});
         set(imfigs,'ButtonDownFcn',@butdwn);
         
     end
@@ -213,7 +251,7 @@ uiwait(multihand);
         
         set(imfigs,'CData',images{selectionid});
         set(imfigs,'ButtonDownFcn',@butdwn);
-
+        title(titles{selectionid});
         set(sliderui,'Value', neworigin )
     end
 end
--- AOTemporalAnalysis/Temporal_Series_Culler/temporal_culler.m
+++ AOTemporalAnalysis/Temporal_Series_Culler/temporal_culler.m
@@ -5,6 +5,7 @@
 % file.
 
 clear
+close all force
 clc
 
 %% Filename determination and handling
@@ -41,7 +42,7 @@ acceptable_frame_fname = [confocal_fname(1:confind-1) acceptable_frame_fname '_a
 acceptable_frame_fname_out = [confocal_fname(1:confind-1) confocal_fname_out '_crop_affine_acceptable_frames.csv'];
 
 % Create our visible output filename
-visible_fname_out = [confocal_fname(1:confind-1) confocal_fname_out '_crop_affine.avi'];
+visible_fname_out = [confocal_fname(1:confind-1) confocal_fname_out '_crop.avi'];
 visible_fname_out = strrep(visible_fname_out, 'confocal', 'visible');
 
 % Create our confocal output filename - affine will need to be done outside
@@ -67,10 +68,42 @@ end
 
 acc_frame_list = dlmread( fullfile(pathname, acceptable_frame_fname) );
 
+acc_frame_list = sort(acc_frame_list);
+
 if length(acc_frame_list) ~= length(confocal_vid)
    error('Acceptable frames and confocal video list lengths do not match!');
 end
 
-[update, selected, multihand] = linkedImageFrameSingle(confocal_vid, frame_nums);
+[selected, cropregion] = linkedImageFrameSingle(confocal_vid, frame_nums);
 
+% Remove the frames that weren't in the list
+confocal_vid = confocal_vid(selected);
+visible_vid = visible_vid(selected);
+acc_frame_list = acc_frame_list(selected);
+
+confocal_vid_out = uint8( zeros( cropregion(3,2)-cropregion(2,2)+1, cropregion(2,1)-cropregion(1,1)+1, length(confocal_vid) ));
+visible_vid_out = uint8( zeros( cropregion(3,2)-cropregion(2,2)+1, cropregion(2,1)-cropregion(1,1)+1, length(confocal_vid) ));
+
+for i=1:length(confocal_vid)
+
+    confocal_vid_out(:,:,i) = uint8(confocal_vid{i}( cropregion(2,2):cropregion(3,2), cropregion(1,1):cropregion(2,1)));
+    visible_vid_out(:,:,i) = uint8(visible_vid{i}( cropregion(2,2):cropregion(3,2), cropregion(1,1):cropregion(2,1)));
+
+end
+
+outfolder = 'region_cropped';
+mkdir(pathname, outfolder);
+
+dlmwrite( fullfile(pathname, outfolder,acceptable_frame_fname_out),acc_frame_list);
+confocal_vidobj = VideoWriter( fullfile(pathname, outfolder, confocal_fname_out), 'Grayscale AVI' );
+visible_vidobj = VideoWriter( fullfile(pathname, outfolder, visible_fname_out), 'Grayscale AVI' );
+
+
+open(confocal_vidobj);
+writeVideo(confocal_vidobj,confocal_vid_out);
+close(confocal_vidobj);
+
+open(visible_vidobj);
+writeVideo(visible_vidobj,visible_vid_out);
+close(visible_vidobj);
 
