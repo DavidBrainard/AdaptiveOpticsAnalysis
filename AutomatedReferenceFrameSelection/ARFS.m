7361c464ec2bd8cae50473369edb7b9e40e8d07f
Robert F Cooper <rfcooper@sas.upenn.edu>
2017-07-14 11:00:00 -0400

    Adjustments to reference selection and distortion script
    - Added static distortion (top of frame/residual) removal to distortion repair.
    - Added working xcorr code to ARFs implementation; need to validate.

--- AutomatedReferenceFrameSelection/ARFS.m
+++ AutomatedReferenceFrameSelection/ARFS.m
@@ -16,7 +16,7 @@ locInd=1; % Location index
 mov_path = {pwd,...
             pwd,...
             pwd};
-stack_fname = {'NC_11049_20170629_confocal_OD_0000_desinusoided.avi',...
+stack_fname = {'NC_11049_20170629_confocal_OD_0000_0003_desinusoided.avi',...
                'NC_11049_20170629_avg_OD_0000_desinusoided.avi',...
                'NC_11049_20170629_split_det_OD_0000_desinusoided.avi'};
 
@@ -45,6 +45,7 @@ for modalityInd=1 : 1%length(stack_fname)
     end
     num_strips = length(strip_inds)-1;
     
+    %% Filter by image mean
     mean_contenders = false(1,numFrames);
     for f=1:numFrames        
         mean_contenders(f) =  (frame_mean(f,modalityInd) < mode_mean(modalityInd)+2*mode_dev(modalityInd)) &&...
@@ -59,6 +60,7 @@ for modalityInd=1 : 1%length(stack_fname)
     %%
     tic;
     if exist('parfor','builtin') == 5 % If we can multithread it, do it!
+        %% Filter by Radon transform FWHM
         parfor f=1:numFrames
             
             frame_ind=frame_contenders(f,modalityInd);
@@ -90,26 +92,15 @@ for modalityInd=1 : 1%length(stack_fname)
         
         threshold = mean(radon_bandwidth(:))+ 2*std(radon_bandwidth(:));
         % After thresholding and removal, update the contenders list.
-        frame_contenders = frame_contenders(~any(radon_bandwidth > threshold,2));
+        frame_contenders = frame_contenders(~any(radon_bandwidth > threshold,2));        
         
+        contender_image_stack = image_stack(:,:, frame_contenders(:, modalityInd), modalityInd);
         
-        frm1 = double(image_stack(:,:, frame_contenders(1, modalityInd), modalityInd));
+        frm1 = double( contender_image_stack(:,:,1) );
         [m, n] = size(frm1);
         paddiffm = (size(frm1,1)*2)-1-m;
         paddiffn = (size(frm1,2)*2)-1-n;
-        
-%         fft_frm = zeros((size(frm1,1)*2)-1,(size(frm1,2)*2)-1, length(frame_contenders));
-%         
-%         parfor f=1:numFrames
-%             frame_ind = frame_contenders(f, modalityInd);
-%             frm = double(image_stack(:,:,frame_ind, modalityInd));
-%             
-%             padfrm = padarray(frm,[paddiffm paddiffn],0,'post');
-%             fft_frm(:,:,f) =  fft2( padfrm );
-%         end
-        
-%         frm1 = double(image_stack(:,:, frame_contenders(1, modalityInd), modalityInd));
-        
+
         % Make a mask to remove any edge effects.
         [maskdistx, maskdisty] = meshgrid( 1:(size(frm1,2)*2)-1, 1:(size(frm1,1)*2)-1);
         
@@ -121,10 +112,6 @@ for modalityInd=1 : 1%length(stack_fname)
         % images at any given point.
         numberOfOverlapPixels = arfs_local_sum(ones(size(frm1)),size(frm1,1),size(frm1,2));
         
-        [m, n] = size(frm1);
-        paddiffm = (size(frm1,1)*2)-1-m;
-        paddiffn = (size(frm1,2)*2)-1-n;
-        
         padfrm1 = padarray(frm1,[paddiffm paddiffn],0,'post');
         
         fft_frm1 =  fft2( padfrm1 );
@@ -132,10 +119,10 @@ for modalityInd=1 : 1%length(stack_fname)
         ncc = zeros(length(frame_contenders)-1,1);
         ncc_offset = zeros(length(frame_contenders)-1,2);
         
+        % This is NOT sped up by multithreading.        
         for f=2:length(frame_contenders)
             
-            frame_ind2 = frame_contenders(f, modalityInd);
-            frm2 = double(image_stack(:,:,frame_ind2, modalityInd));
+            frm2 = double(contender_image_stack(:,:,f));
                          
             padfrm2 = padarray(frm2,[paddiffm paddiffn],0,'post'); 
             
@@ -160,40 +147,130 @@ for modalityInd=1 : 1%length(stack_fname)
             % Numerator for NCC
             numerator = fftshift( real(ifft2( fft_frm1 .* conj( fft_frm2 ) ) ));
             
-%             pcorr= ifft2( (fft_frm1 .* conj( fft_frm2 ))./abs(fft_frm1 .* conj( fft_frm2 )) );
-            
             numerator = numerator - local_sum_F.*local_sum_T./numberOfOverlapPixels;
             
             denom = sqrt(denom_F.*denom_T);
             
             ncc_frm = (numerator./denom);
+            ncc_frm(isnan(ncc_frm)) =0;
             
+            stddev_ncc_frm = stdfilt(ncc_frm,ones(11)).*xcorr_mask;
             masked_ncc_frm = (ncc_frm.*xcorr_mask);
-            masked_ncc_frm = masked_ncc_frm +( (~xcorr_mask).* min(masked_ncc_frm(:)) );
+            masked_ncc_frm(isnan(masked_ncc_frm)) =0;
             
             [ncc(f-1), ncc_ind_offset]  = max(masked_ncc_frm(:));
             
             [xoff, yoff]= ind2sub(size(masked_ncc_frm),ncc_ind_offset);                        
             ncc_offset(f-1,:) = [xoff yoff];
             
-%             if f==65
-%                 figure(1);imshowpair(frm1,frm2,'montage')
-%                 mean2(frm2)
-%                 figure(2);imagesc(masked_ncc_frm); axis image;
-%                 [thexcorr,nft]=normxcorr2_general(frm2,frm1);
-%                 figure;imagesc(ncc_frm-thexcorr);
-%             end
+            if f>56
+                figure(1);imshowpair(frm1,frm2,'montage')
+                figure(2);imagesc(masked_ncc_frm); axis image;
+                figure(3); imagesc(stddev_ncc_frm);
+                
+%                 std(stddev_ncc_frm)
+            end
             
             % Frame 2 is now frame 1
             fft_frm1 = fft_frm2;
-            frm1 = frm2;
+            frm1 = frm2;            
+        end
+        toc;
+        
+        %% Filter by neighbor NCC
+        % When calculating the ncc threshold, only use sequential frames (further separated in time isn't fair).
+        sequential_frames = diff(frame_contenders)==1;        
+        ncc_threshold = median(ncc(sequential_frames));
+        
+        seq_ncc = ncc(sequential_frames);
+        
+        hist(seq_ncc,20); hold on; plot([ncc_threshold ncc_threshold],[0 10],'r'); hold off;
+                
+        ncc_offset(:,1)=ncc_offset(:,1)-size(contender_image_stack(:,:,1),1);
+        ncc_offset(:,2)=ncc_offset(:,2)-size(contender_image_stack(:,:,1),2); 
+        
+        absolute_offsets = cumsum(ncc_offset);
+        
+        rem_voting = nan(size(frame_contenders));
+        voting_capacity = zeros(size(frame_contenders));               
+        % Move a sliding window along the ncc values and determine which
+        % frames consistently have poor NCC with their surrounding
+        % neighbors.
+        % Reminder: Each NCC value is a comparison between two frames
+        for f=1:length(ncc)
+            if (f-1 ~= 0) && (frame_contenders(f)-frame_contenders(f-1) == 1) % Between f-1 and f
+                
+                atthresh = ncc(f-1) < ncc_threshold;
+                
+                if isnan(rem_voting(f-1))
+                    rem_voting(f-1) = 0;
+                end
+                if isnan(rem_voting(f))
+                    rem_voting(f) = 0; 
+                end
+                
+                voting_capacity(f-1)=voting_capacity(f-1)+1;
+                voting_capacity(f)= voting_capacity(f)+1;
+                % Add weighted votes between the frame of interest and previous frame
+                rem_voting(f-1) = rem_voting(f-1) + (ncc(f-1) < ncc_threshold);
+                rem_voting(f) = rem_voting(f) + (ncc(f-1) < ncc_threshold);
+            else
+%                 frame_contenders(f)
+            end
             
+            if (f+1 < length(frame_contenders)) && (frame_contenders(f+1)-frame_contenders(f) == 1) % Between f and f+1
+                atthresh = ncc(f) < ncc_threshold;
+                
+                if isnan(rem_voting(f))
+                    rem_voting(f) = 0; 
+                end
+                if isnan(rem_voting(f+1))
+                    rem_voting(f+1) = 0; 
+                end
+                
+                voting_capacity(f) = voting_capacity(f)+1;
+                voting_capacity(f+1)= voting_capacity(f+1)+1;
+                % Add weighted votes between the frame of interest and next frame
+                rem_voting(f) = rem_voting(f) + (ncc(f) < ncc_threshold);
+                rem_voting(f+1) = rem_voting(f+1) + (ncc(f) < ncc_threshold); 
+            else
+%                 frame_conttenders(f)
+            end            
         end
         
-        ncc_offset(:,1)=ncc_offset(:,1)-size(image_stack(:,:,1),1);
-        ncc_offset(:,2)=ncc_offset(:,2)-size(image_stack(:,:,1),2);
+        % VALIDATE THIS!
+        to_remove = rem_voting == voting_capacity;
+        remove_by_vote_paired = true(length(to_remove)-1,1);
+        for v=2 :length(to_remove)
+            if to_remove(v-1) || to_remove(v)
+                remove_by_vote_paired(v-1) = false;
+            end
+        end
+        
+        figure; plot(absolute_offsets(:,1), absolute_offsets(:,2),'.'); title('Before removal');
+        
+        % If you recieved all the votes you could to get removed, then you get dropped.
+        frame_contenders = frame_contenders(rem_voting ~= voting_capacity);
+        ncc = ncc(remove_by_vote_paired);
+        ncc_offset = ncc_offset(remove_by_vote_paired,:);
+        absolute_offsets = absolute_offsets(remove_by_vote_paired,:);
+        
+        figure; plot(absolute_offsets(:,1), absolute_offsets(:,2),'.'); title('After removal');
+        % If we can't be sure you're a good frame, then you get dropped.
+%         voting_capacity = voting_capacity(rem_voting ~= voting_capacity);
+%         frame_contenders = frame_contenders(voting_capacity ~= 2);
+%         ncc = ncc(rem_voting ~= voting_capacity);
+%         ncc_offset = ncc_offset(voting_capacity ~= 2,:);
+        
 
-        figure; plot(cumsum(ncc_offset(:,1)),cumsum(ncc_offset(:,2)),'.');
+        
+        
+        for f=2:length(ncc_offset)            
+            transim = imtranslate(contender_image_stack(:,:,f),[ncc_offset(f-1,2), ncc_offset(f-1,1)],'FillValues',0);        
+            figure(2);imshowpair(transim,contender_image_stack(:,:,f-1));
+            title(num2str(ncc(f-1)));
+            pause;
+        end
         
         
         
---
+++ Distortion_Removal/AOSLO_Frame_Distortion_Analysis.m
@@ -0,0 +1,20 @@
+
+
+clear;
+close all;
+clc
+
+[distortfile, distortpath]=uigetfile('*.mat','Select the desinusoid file for this dataset.');
+
+load(fullfile(distortpath,distortfile),'horizontal_fringes_indices_minima');
+
+static_grid_distortion = AOSLO_Static_Distortion_Repair(horizontal_fringes_indices_minima);
+
+motion_path = uigetdir(pwd);
+
+fNames = read_folder_contents(motion_path,'tif');
+
+parfor i=1:length(fNames)
+    i
+    AOSLO_Eye_Motion_Distortion_Repair(motion_path, fNames{i}, static_grid_distortion);
+end
