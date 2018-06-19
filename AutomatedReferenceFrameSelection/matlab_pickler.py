1298e4b23cedd7274c75bda0a15e2c4f43818aec
Robert F Cooper <rfcooper@sas.upenn.edu>
2018-06-19 08:31:19 -0400

    Updates to pickler behavior to actually output dmb files.

--- AutomatedReferenceFrameSelection/matlab_pickler.py
+++ AutomatedReferenceFrameSelection/matlab_pickler.py
@@ -4,11 +4,14 @@ import os,pickle
 
 
 def pickle_mat(pickle_path, this_dict):
+	
 
-    wut=numpy.reshape(this_dict['desinusoid_matrix'], (this_dict['n_columns_raw_sequence'], this_dict['n_columns_desinusoided'] ) )
-    print len(wut)
-    #pickle_file = open(pickle_path, 'wb')
+    this_dict['desinusoid_matrix']=numpy.reshape(this_dict['desinusoid_matrix'], (int(this_dict['n_columns_desinusoided']), int(this_dict['n_columns_raw_sequence']) ) )
+    this_dict['desinusoid_matrix']=numpy.transpose(this_dict['desinusoid_matrix'])
+    print this_dict['desinusoid_matrix']
+    print this_dict['desinusoid_matrix'].shape
+    pickle_file = open(pickle_path, 'wb')
     
-    #pickle.dump(this_dict,pickle_file)
+    pickle.dump(this_dict,pickle_file)
 
-    #pickle_file.close()
+    pickle_file.close()
