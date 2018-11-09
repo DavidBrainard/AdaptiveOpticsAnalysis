f9627365c9961b417911bf192074a07d68ff0b19
David Brainard <brainard@psych.upenn.edu>
2018-11-09 15:50:05 -0500

    Closer to working now.

--- AOLightLevels/AO_Bleaching_Kinetics.m
+++ AOLightLevels/AO_Bleaching_Kinetics.m
@@ -5,14 +5,14 @@
 % experiments.
 
 clear;
-close all force;
+%close all force;
 
 stim_lambda = 550; % in nm
-stim_irradiance = 0.450; % in uW
+stim_irradiance = 2*0.00450; % in uW
 
 num_acquisitions = 13;
-single_trial_train = [0 1 0;
-                      4 6 20];
+single_trial_train = [0 1  0;
+                      4 200 201];
 
 trial_train = zeros(2, size(single_trial_train,2).*num_acquisitions);
 for n=1:num_acquisitions
@@ -31,14 +31,15 @@ end
 
 
 [~,I] = AOLightLevelConversions_Func(1, stim_lambda, stim_irradiance, true); % Stimulus Intensity in Td
-I = log(I);
-I_0 = log(20000); %in Td %Stimulus intensity that bleaches at the rate of 1/N
+%I = log10(I);
+%I_0 = log10(20000); %in Td %Stimulus intensity that bleaches at the rate of 1/N
             % 73.7 Td for rods, 20000 Td for cones
+I_0 = 20000;
 
 N = 120; % Scaling factor, where 400=rhodopsin, 120=L/M cones.
-p = 1; % Percentage of bleached pigment that we start with. Assume dark adapted.
+p = 1; % Percentage of unbleached pigment that we start with. Assume dark adapted.
 
-dt = 0.01;
+dt = 0.001;
 
 dp_dt_deplete = @(p, I) (( (1-p)./N) - ( (I.*p)./ (N.*I_0) ));
 dp_dt_recover = @(p, I) ( (1-p)./N );
@@ -55,9 +56,10 @@ for t=1:length(time)
     end
     
     if trial_train(1, train_ind) == 1
-        bleach_curve(t) = p + dp_dt_deplete(p,I);
+        %bleach_curve(t) = p + dp_dt_deplete(p,I)*dt;
+        bleach_curve(t) = p + (( (1-p)./N) - ( (I.*p)./ (N.*I_0) ))*dt;
     else
-        bleach_curve(t) = p + dp_dt_recover(p,I);        
+        bleach_curve(t) = p + dp_dt_recover(p,I)*dt;        
     end
     p = bleach_curve(t); % Update our bleach percentage.
 end
